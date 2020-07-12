23def900ab9602e3642ba808093c7168ddd0552c
Alen Rafagudinov <rafagudinov@gmail.com>
2020-07-12 18:50:38 +0300

    Refactor environment variables and Docker entry point

--- {{cookiecutter.project_slug}}/app/docker-entrypoint.sh
+++ {{cookiecutter.project_slug}}/app/docker-entrypoint.sh
@@ -5,24 +5,23 @@ while ! mysqladmin ping -h ${DB_HOST:-{{cookiecutter.project_slug}}_db} --silent
     sleep 1
 done
 
-case $ENV_TYPE in
-prod)
-    echo "Starting application using Gunicorn server (production environment)"
-    flask db upgrade head
-    exec gunicorn --bind 0.0.0.0:80 --workers=2 ${@:2} app:app
-    ;;
-dev)
-    echo "Starting application using Flask server (development environment)"
-    flask db upgrade head
-    export FLASK_ENV=development
-    export FLASK_RUN_EXTRA_FILES={{cookiecutter.project_slug}}/api/api.yaml
-    exec flask run --host=0.0.0.0 --port=80 ${@:2}
-    ;;
-test)
+case $1 in
+tests)
     echo "Running tests"
-    exec pytest tests/${SELECTED_TESTS}
+    exec pytest tests/${@:2}
     ;;
 *)
-    echo "Set ENV_TYPE to 'prod', 'dev' or 'test'"
+    flask db upgrade head
+
+    if [ "$ENV_TYPE" = "dev" ];
+    then
+        echo "Starting application using Flask server (development environment)"
+        export FLASK_ENV=development
+        export FLASK_RUN_EXTRA_FILES={{cookiecutter.project_slug}}/api/api.yaml
+        exec flask run --host=0.0.0.0 --port=80 ${@:2}
+    else
+        echo "Starting application using Gunicorn server (production environment)"
+        exec gunicorn --bind 0.0.0.0:80 --workers=2 ${@:2} app:app
+    fi
     ;;
 esac
